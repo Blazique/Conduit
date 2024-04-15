be4b25e7f8174293403972287279f47bfb702f24
Maurice CGP Peters <me@mauricepeters.dev>
2024-04-15 10:15:13 +0200

    Replace App.razor and Routes.razor with Blazique equivalents

--- Conduit/Components/App.cs
+++ Conduit/Components/App.cs
@@ -1,10 +1,3 @@
-using Blazique;
-using Radix;
-using System.Runtime.CompilerServices;
-using static Conduit.Components.Elements;
-using static Conduit.Components.Attributes;
-
-using Conduit.Components.Names.Elements;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace Conduit.Components;
@@ -14,7 +7,8 @@ public class App : Blazique.Web.Component
 {
     public override Node[] Render() =>
         [
-            doctype([Attributes.html([])], []),
+            doctype([Attributes.html([])]
+            ),
             html([lang(["en"])], []),
             head([], [
                 meta([charset(["utf-8"])], []),
@@ -29,22 +23,28 @@ public class App : Blazique.Web.Component
                 component<Routes>([], []),
                 script([src(["_framework/blazor.web.js"])], []),
             ])
-        ];
-
-    
-
-    
+        ];   
 }
 
-public static class Elements
-{
-    public static Node doctype(Blazique.Data.Attribute[] attributes, Node[] children, object? key = null, [CallerLineNumber] int nodeId = 0) =>
-        Element.Create<doctype>(attributes, children, key, nodeId);
+// The original blazor razor file content:
 
-}
+// <!DOCTYPE html>
+// <html lang="en">
 
-public static class Attributes
-{
-    public static Blazique.Data.Attribute html(string[] values, [CallerLineNumber] int nodeId = 0) =>
-        Blazique.Attribute.Create<Conduit.Components.Names.Attributes.html>(values, nodeId);
-}
+// <head>
+//     <meta charset="utf-8" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <base href="/" />
+//     <link rel="stylesheet" href="bootstrap/bootstrap.min.css" />
+//     <link rel="stylesheet" href="app.css" />
+//     <link rel="stylesheet" href="BlazorDefault.styles.css" />
+//     <link rel="icon" type="image/png" href="favicon.png" />
+//     <HeadOutlet />
+// </head>
+
+// <body>
+//     <Routes />
+//     <script src="_framework/blazor.web.js"></script>
+// </body>
+
+// </html>
--- Conduit/Components/Routes.cs
+++ Conduit/Components/Routes.cs
@@ -1,13 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components;
-using Blazique;
-using Radix;
-using System.Runtime.CompilerServices;
-using static Conduit.Components.Elements;
-using static Conduit.Components.Attributes;
-
-using Conduit.Components.Names.Elements;
-using Microsoft.AspNetCore.Components.Web;
+using static Blazique.Attribute;
 
 namespace Conduit.Components;
 
@@ -21,31 +14,11 @@ public class Routes : Blazique.Web.Component
                     component<RouteView>([attribute("RouteData", [routeData]), attribute("DefaultLayout", [typeof(Layout.MainLayout)])], []),
                     component<FocusOnNavigate>([attribute("RouteData", [routeData]), attribute("Selector", ["h1"])], [])
                 ])
-            ],[])
-            
+            ],[])            
         ];
-        
-
-    public static Blazique.Data.Attribute attribute(string name, object[] values, [CallerLineNumber] int nodeId = 0) =>
-        Blazique.Attribute.Create(name, values, nodeId);
-
-    public static Blazique.Data.Attribute fragment(string name, Node[] node, [CallerLineNumber] int nodeId = 0) => 
-    (component, builder) =>
-        builder.AddAttribute(nodeId, name, new RenderFragment(rt => {
-            foreach (var n in node)
-                n.Invoke(component, rt);
-        }));
-
-    public static Blazique.Data.Attribute fragment<T>(string name, Func<T, Node[]> f, [CallerLineNumber] int nodeId = 0) => 
-    (component, builder) =>
-        builder.AddAttribute(nodeId, name, new RenderFragment<T>(context => 
-            new RenderFragment(rt => 
-            {
-                var node = f(context);
-                foreach (var n in node)
-                    n.Invoke(component, rt);
-            })));
 }
+
+// The original blazor razor file content:
 
 // < Router AppAssembly = "typeof(Program).Assembly" >
 //    < Found Context = "routeData" >
